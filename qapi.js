5fa691422141ea049aec1f7d3abde763301fa7f6
Alexander <webisforce@gmail.com>
2021-06-07 15:02:43 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60be0aadd3d3a2001e2abff4",
+				"60be0aadd3d3a2001e2abff6"
+			]
+		},
+		"60be0aadd3d3a2001e2abff4": {
+			"id": "60be0aadd3d3a2001e2abff4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60be0aadd3d3a2001e2abff6": {
+			"id": "60be0aadd3d3a2001e2abff6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/Cookie.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import atomize from '@quarkly/atomize';
+import { Box, Button, Text } from '@quarkly/widgets';
+import { useOverrides } from '@quarkly/components';
+const item = 'accept_cookies';
+
+const getAPI = () => {
+	if (typeof window !== "undefined") {
+		return window.QAPI || {};
+	}
+
+	if (typeof global !== "undefined") {
+		return global.QAPI || {};
+	}
+
+	return {};
+};
+
+const overrides = {
+	Text: {
+		kind: 'Text',
+		props: {
+			// Мы используем cookies для того, чтобы обеспечить наилучшую работу нашего сайта. Продолжая использовать наш сайт, Вы соглашаетесь с использованием cookie-файлов.
+			children: 'We use cookies to improve your experience on our site. By using our site, you consent to out use of cookies.'
+		}
+	},
+	Button: {
+		kind: 'Button',
+		props: {
+			children: 'Accept'
+		}
+	}
+};
+
+const getDefaultState = isDev => {
+	if (typeof window === 'undefined') return false;
+	return !localStorage.getItem(item) || isDev;
+};
+
+const CookieUsed = ({
+	variant,
+	...props
+}) => {
+	const isDev = getAPI().mode === 'development';
+	const [show, setShow] = useState(getDefaultState(isDev));
+	const {
+		override,
+		children,
+		rest
+	} = useOverrides(props, overrides);
+
+	const handleClick = () => {
+		if (isDev) return;
+		localStorage.setItem(item, true);
+		setShow(false);
+	};
+
+	return <Box
+		position="fixed"
+		width="100%"
+		bottom="0"
+		display={show ? 'flex' : 'none'}
+		flex-direction={variant === "Vertical" ? 'column' : 'row'}
+		justify-content="center"
+		align-items="center"
+		background="--color-primary"
+		padding="10px 0"
+		{...rest}
+	>
+		    
+		<Text color="--color-light" vertical-align="middle" {...override('Text')} />
+		    
+		<Button
+			margin-left={variant === 'Horizontal' && '10px'}
+			color="--color-primary"
+			background="--color-light"
+			onClick={handleClick}
+			{...override('Button')}
+		/>
+		  
+	</Box>;
+};
+
+const propInfo = {
+	variant: {
+		control: "radio-group",
+		variants: ["Vertical", "Horizontal"]
+	}
+};
+const defaultProps = {
+	variant: "Vertical"
+};
+export default atomize(CookieUsed)({
+	name: 'CookieUsed',
+	effects: {
+		hover: ':hover'
+	},
+	overrides,
+	description: {
+		en: 'This component notifies users about the use of cookies.'
+	},
+	propInfo
+}, defaultProps);
